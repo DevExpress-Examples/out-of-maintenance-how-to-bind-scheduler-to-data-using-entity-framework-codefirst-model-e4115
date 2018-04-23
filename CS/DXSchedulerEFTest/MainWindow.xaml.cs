9e8cd3ae350fcb0d88a6c711f69e48d437e3618e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXSchedulerEFTest/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿#region #mainwindow
+using System;
+using System.Data.Entity;
+using System.Windows;
+using DevExpress.XtraScheduler;
+
+namespace DXSchedulerEFTest
+{   
+    public partial class MainWindow : Window
+    {
+        private SchedulerContext context = new SchedulerContext();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Database.SetInitializer(new SchedulerContextSeedInilializer());
+            context.Database.Initialize(false);
+            context.EFAppointments.Load();
+            context.EFResources.Load();
+
+            this.schedulerControl1.Storage.AppointmentStorage.DataContext = context.EFAppointments.Local;
+            this.schedulerControl1.Storage.ResourceStorage.DataContext = context.EFResources.Local;
+
+            this.schedulerControl1.Storage.AppointmentsInserted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            this.schedulerControl1.Storage.AppointmentsChanged +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            this.schedulerControl1.Storage.AppointmentsDeleted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+
+            schedulerControl1.Storage.ResourceStorage.ColorSaving = ColorSavingType.ArgbColor;
+            schedulerControl1.Storage.AppointmentStorage.ResourceSharing = true;
+            schedulerControl1.GroupType = SchedulerGroupType.Resource;
+            schedulerControl1.Start = DateTime.Now.Date;
+        }
+
+        void Storage_AppointmentsModified(object sender, PersistentObjectsEventArgs e)
+        {
+            context.SaveChanges();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this.context.Dispose();
+        }
+    }
+}
+#endregion #mainwindow
---
+++ VB/DXSchedulerEFTest/MainWindow.xaml.vb
@@ -0,0 +1,45 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#mainwindow"
+Imports System
+Imports System.Data.Entity
+Imports System.Windows
+Imports DevExpress.XtraScheduler
+
+Namespace DXSchedulerEFTest
+	Partial Public Class MainWindow
+		Inherits Window
+		Private context As New SchedulerContext()
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Database.SetInitializer(New SchedulerContextSeedInilializer())
+			context.Database.Initialize(False)
+			context.EFAppointments.Load()
+			context.EFResources.Load()
+
+			Me.schedulerControl1.Storage.AppointmentStorage.DataContext = context.EFAppointments.Local
+			Me.schedulerControl1.Storage.ResourceStorage.DataContext = context.EFResources.Local
+
+			AddHandler schedulerControl1.Storage.AppointmentsInserted, AddressOf Storage_AppointmentsModified
+			AddHandler schedulerControl1.Storage.AppointmentsChanged, AddressOf Storage_AppointmentsModified
+			AddHandler schedulerControl1.Storage.AppointmentsDeleted, AddressOf Storage_AppointmentsModified
+
+			schedulerControl1.Storage.ResourceStorage.ColorSaving = ColorSavingType.ArgbColor
+			schedulerControl1.Storage.AppointmentStorage.ResourceSharing = True
+			schedulerControl1.GroupType = SchedulerGroupType.Resource
+			schedulerControl1.Start = DateTime.Now.Date
+		End Sub
+
+		Private Sub Storage_AppointmentsModified(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+			context.SaveChanges()
+		End Sub
+
+		Protected Overrides Sub OnClosing(ByVal e As System.ComponentModel.CancelEventArgs)
+			MyBase.OnClosing(e)
+			Me.context.Dispose()
+		End Sub
+	End Class
+End Namespace
+#End Region ' #mainwindow
