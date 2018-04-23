da5f005bfe9150ba47b30016dd7a8afbd4eee30a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to bind Scheduler to data using Entity Framework CodeFirst model
+
+
+<p>This example illustrates how to create the model of appointments and resources by using Code First development and then use the types defined in the model as data sources for the SchedulerStorage component in the DXScheduler for WPF application.<br /><br /><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-bind-scheduler-to-data-using-entity-framework-codefirst-model-e4115/14.2.3+/media/73159094-cbed-11e4-80bf-00155d62480c.png"></p>
+
+<br/>
+
+
---
+++ VB/DXSchedulerGettingStarted/MainWindow.xaml.vb
@@ -0,0 +1,44 @@
+﻿#Region "#usings"
+Imports System.Windows
+Imports System.Data.Entity
+Imports DevExpress.XtraScheduler
+Imports DevExpress.Xpf.Ribbon
+#End Region ' #usings
+Namespace DXSchedulerGettingStarted
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits DXRibbonWindow
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        #Region "#window_loaded"
+        Private context As New MySchedulerModel()
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            context.MyAppointments.Load()
+            context.MyResources.Load()
+
+            Me.scheduler.Storage.AppointmentStorage.DataContext = context.MyAppointments.Local
+            Me.scheduler.Storage.ResourceStorage.DataContext = context.MyResources.Local
+
+
+            AddHandler scheduler.Storage.AppointmentsInserted, AddressOf Storage_AppointmentsModified
+            AddHandler scheduler.Storage.AppointmentsChanged, AddressOf Storage_AppointmentsModified
+            AddHandler scheduler.Storage.AppointmentsDeleted, AddressOf Storage_AppointmentsModified
+        End Sub
+        #End Region ' #window_loaded
+        #Region "#storage_appointmentsmodified"
+        Private Sub Storage_AppointmentsModified(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+            context.SaveChanges()
+        End Sub
+        #End Region ' #storage_appointmentsmodified
+        #Region "#onclosing"
+        Protected Overrides Sub OnClosing(ByVal e As System.ComponentModel.CancelEventArgs)
+            MyBase.OnClosing(e)
+            Me.context.Dispose()
+        End Sub
+        #End Region ' #onclosing
+    End Class
+End Namespace
